e53139835d25b00cd43985ef179aed6a5854a5fc
Nick Marble <nmarble@uccs.edu>
2019-08-26 20:13:30 -0600

    Added Maven Project

---
+++ src/main/java/com/gomengaming/frolictwitch/FrolicTwitchApplication.java
@@ -0,0 +1,13 @@
+package com.gomengaming.frolictwitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrolicTwitchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrolicTwitchApplication.class, args);
+    }
+
+}
